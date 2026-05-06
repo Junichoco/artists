9bc1de909cf9c03b937ee4937cb569fc83b8af58
unknown <sunjun.software@gmail.com>
2026-05-07 01:23:29 +0900

    added artists

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ function App() {
     <div className='container'>
       <h1>Artists</h1>
       <div className='section-container'>
-        <h2>coconala</h2>
-        <h3>Video</h3>
+        <h2>Video</h2>
+        <h3>coconala</h3>        
         <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/1641271">youmu869</a><br/>
         <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/1385200">天【動画編集】</a><br/>
         <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/2095083">xこめちゃんx</a>
       </div>
       <div className='section-container'>
-        <h2>skeb</h2>
+        <h2>Art</h2>
+        <skeb></skeb>
         <a target="_blank" and rel="noopener noreferrer" href="https://skeb.jp/@3Tokitamago3">とかれたたまご</a><br/>
-
+        <h3>Twitter</h3>
         <a target="_blank" and rel="noopener noreferrer" href="https://lit.link/en/shiropon">Tukiyume shiro</a><br/>
         <img src="Tsukiyume Shiro.png"></img>
+        <h3>Skima</h3>
+        <a target="_blank" and rel="noopener noreferrer" href="https://skima.jp/profile?id=115376">RamRom Labo</a><br/>
         <p></p>
       </div>
     </div>
