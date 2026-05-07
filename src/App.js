244c27578f5f0df396bb30ed3285842ebd4a73a6
unknown <sunjun.software@gmail.com>
2026-05-07 19:52:29 +0900

    added

--- src/App.js
+++ src/App.js
@@ -5,23 +5,62 @@ function App() {
     <div className='container'>
       <h1>Artists</h1>
       <div className='section-container'>
+        <h2>Music</h2>
+        <h3>coconala</h3>
+          <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/2556499">マクロネコ</a><br/>
+          <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/1194588">scots</a><br/>
+          <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/2621923">wagataki</a><br/>
+
+          
+      </div>
+      <div className='section-container'>
         <h2>Video</h2>
         <h3>coconala</h3>        
         <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/1641271">youmu869</a><br/>
         <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/1385200">天【動画編集】</a><br/>
-        <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/2095083">xこめちゃんx</a>
+        <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/2095083">xこめちゃんx</a><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/1677514">マサキ</a><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/2845259">まきはらまき</a><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/2830549">anz create</a><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/1385200">天</a><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://coconala.com/services/3136649">エルシー</a><br/>
+
+
       </div>
       <div className='section-container'>
         <h2>Art</h2>
         <skeb></skeb>
         <a target="_blank" and rel="noopener noreferrer" href="https://skeb.jp/@3Tokitamago3">とかれたたまご</a><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://skeb.jp/@kpkp_bon">くらむぼん</a><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://skeb.jp/@ko_cha_22">初白まみむ</a><br/>
+        <div className='image-container'>
+          <img src="mamimu 1.avif" alt=""></img>
+          <img src="mamimu 2.avif" alt=""></img>
+          <img src="mamimu 3.avif" alt=""></img>
+        </div>
+        <a target="_blank" and rel="noopener noreferrer" href="https://skeb.jp/@369_EW">シュークリームどうぶつえん</a><br/>
+        
         <h3>Twitter</h3>
         <a target="_blank" and rel="noopener noreferrer" href="https://lit.link/en/shiropon">Tukiyume shiro</a><br/>
-        <img src="Tsukiyume Shiro.png"></img>
+        <img src="Tsukiyume Shiro.png" alt=""></img><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://lit.link/en/gomapiyo">ごまぴよ</a><br/>
+        
+
+
+        
+        
         <h3>Skima</h3>
         <a target="_blank" and rel="noopener noreferrer" href="https://skima.jp/profile?id=115376">RamRom Labo</a><br/>
         <p></p>
       </div>
+      <div className='section-container'>
+        <h2>Animations</h2>
+        <a target="_blank" and rel="noopener noreferrer" href="https://skeb.jp/@k_szk">すずか</a><br/>
+        <a target="_blank" and rel="noopener noreferrer" href="https://skeb.jp/@popotowari">ぽとわり</a><br/>
+
+      </div>
+
+      
     </div>
   );
 }
